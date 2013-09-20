8fc395e61378359a307fb4bb30bd1727bdab969f
Ric Allinson <capecodehq@yahoo.co.uk>
2013-09-19 21:09:08 -0700

    New idea

--- forgery.go
+++ forgery.go
@@ -4,9 +4,20 @@ import(
     "github.com/ricallinson/stackr"
 )
 
+type Server struct {
+    *stackr.Server
+}
+
 /*
-    Create forgery Request, Response types.
+    Create a new stackr server.
 */
-func Make(req *stackr.Request, res *stackr.Response) (*Request, *Response) {
+func CreateServer() (*Server) {
+    return &Server{&stackr.Server{}}
+}
+
+/*
+    Create forgery Request, Response types from stackr Request, Response types.
+*/
+func make(req *stackr.Request, res *stackr.Response) (*Request, *Response) {
     return createRequest(req), createResponse(res)
 }
