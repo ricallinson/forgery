db533dbab4908df6a3c0663d39230b9e8c06ef62
Ric Allinson <capecodehq@yahoo.co.uk>
2013-11-19 15:44:16 +0800

    Added convenience attributes for accessing stackr middleware.

---
+++ stackr.go
@@ -0,0 +1,20 @@
+package f
+
+import(
+    "github.com/ricallinson/stackr"
+)
+
+/*
+    Convenience attribute for accessing "stackr.Static".
+*/
+var Static = stackr.Static
+
+/*
+    Convenience attribute for accessing "stackr.Favicon".
+*/
+var Favicon = stackr.Favicon
+
+/*
+    Convenience attribute for accessing "stackr.Logger".
+*/
+var Logger = stackr.Logger
