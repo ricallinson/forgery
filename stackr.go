d869a3473a0c23e96e05e933fa1fe6b4db03a442
Ric Allinson <capecodehq@yahoo.co.uk>
2013-11-19 21:24:52 +0800

    Go fmt

--- stackr.go
+++ stackr.go
@@ -1,25 +1,25 @@
 package f
 
-import(
-    "github.com/ricallinson/stackr"
+import (
+	"github.com/ricallinson/stackr"
 )
 
 /*
-    Convenience attribute for accessing "stackr.Favicon".
+   Convenience attribute for accessing "stackr.Favicon".
 */
 var Favicon = stackr.Favicon
 
 /*
-    Convenience attribute for accessing "stackr.Logger".
+   Convenience attribute for accessing "stackr.Logger".
 */
 var Logger = stackr.Logger
 
 /*
-    Convenience attribute for accessing "stackr.ResponseTime".
+   Convenience attribute for accessing "stackr.ResponseTime".
 */
 var ResponseTime = stackr.ResponseTime
 
 /*
-    Convenience attribute for accessing "stackr.Static".
+   Convenience attribute for accessing "stackr.Static".
 */
 var Static = stackr.Static
