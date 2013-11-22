bbb323d61c8434a4546eb8bcf7582039cf8a19f0
Ric Allinson <capecodehq@yahoo.co.uk>
2013-11-22 20:54:01 +0800

    Update to comments

--- router.go
+++ router.go
@@ -20,10 +20,16 @@ type Router struct {
 	Strict bool
 }
 
+/*
+	Add a "param" function to the Router.
+*/
 func (this *Router) AddParamFunc(name string, fn func(*Request, *Response, func())) {
 	this.ParamFuncs[name] = fn
 }
 
+/*
+	Add a new route to the Router.
+*/
 func (this *Router) AddRoute(verb string, path string, funcs ...func(*Request, *Response, func())) {
 
 	route := &Route{
@@ -40,30 +46,32 @@ func (this *Router) AddRoute(verb string, path string, funcs ...func(*Request, *
 /*
 	Calls the function defined for each key in "req.Params" if available.
 */
-func (this *Router) executeParamFuncs(req *Request, res *Response, next func()) {
-	// Call param functions if they are set.
+func (this *Router) executeParamFuncs(req *Request, res *Response, next func()) (bool) {
+	// Call each "param" function if one is set.
 	for name := range req.Params {
-		if fn, ok := this.ParamFuncs[name]; ok {
-			fn(req, res, next)
+		if pfn, ok := this.ParamFuncs[name]; ok {
+			pfn(req, res, next)
 			// If the response has been "closed" by a "param" function then return.
 			if res.Closed == true {
-				return
+				return true
 			}
 		}
 	}
+	return false
 }
 
 /*
 	Calls each function defined in the given route.
 */
-func (this *Router) executeRouteFuncs(req *Request, res *Response, next func(), route *Route) {
-	for _, fn := range route.Funcs {
-		fn(req, res, next)
+func (this *Router) executeRouteFuncs(req *Request, res *Response, next func(), route *Route) (bool) {
+	for _, rfn := range route.Funcs {
+		rfn(req, res, next)
 		// If the response has been "closed" by a "route" function then return.
 		if res.Closed == true {
-			return
+			return true
 		}
 	}
+	return false
 }
 
 func (this *Router) Middleware(app *Server) func(req *stackr.Request, res *stackr.Response, next func()) {
@@ -93,9 +101,15 @@ func (this *Router) Middleware(app *Server) func(req *stackr.Request, res *stack
 				// Set the route "params" found in the path.
 				freq.Params = params
 				// Call "param" functions if they are defined.
-				this.executeParamFuncs(freq, fres, next)
+				// If it returns true then the connection has closed so stop processing.
+				if this.executeParamFuncs(freq, fres, next) {
+					return
+				}
 				// Call "route" functions if they are defined.
-				this.executeRouteFuncs(freq, fres, next, route)
+				// If it returns true then the connection has closed so stop processing.
+				if this.executeRouteFuncs(freq, fres, next, route) {
+					return
+				}
 			}
 		}
 	}
